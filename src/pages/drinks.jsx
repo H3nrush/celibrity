16a94ad9aed04b9cce1e9c58facaa697c01f3bb7
hassan <hassanmohammadi078@gmail.com>
2023-11-23 14:15:03 +0100

    fetch

--- src/pages/drinks.jsx
+++ src/pages/drinks.jsx
@@ -3,11 +3,15 @@ import Header from "../component/Header/Header";
 
 function Drinks (){
 
+  //i wanna recieve a data if the drinks for ex: for bar so first i will add the pachage url
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=")
+    //then in here iwill say while you recieve answer from url add the lists of value into a json file 
     .then((answer) => {
       return answer.json();
     })
+    //then here i will say to log me every drinks from the list . here json is for every drink 
     .then((json) => {
+      // now i test whith console.log
       console.log(json);
     });
 

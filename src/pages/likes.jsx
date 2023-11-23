0b93dac287c8b0105840da5a3f0c62fd00baa777
hassan <hassanmohammadi078@gmail.com>
2023-11-23 11:57:55 +0100

    massege liker

--- src/pages/likes.jsx
+++ src/pages/likes.jsx
@@ -5,11 +5,16 @@ function Likes (){
     //here i make two const one for rechange the value and one to type the value in tags 
     
     const [likesCount , setLikesCount] = useState(0);
+    const [Massege , setMassege] = useState(null);
     //useState will give a value for both and reshare the value between two const
     // so here i explain to the const everytime that i click on the btn give me one value of 1+
 
     const likesClick = () => {
+        if (likesCount < 5){
         setLikesCount(likesCount+1);
+    }else{
+        setMassege("you are liking a lot")
+    }
     }
     
 
@@ -24,6 +29,7 @@ function Likes (){
         <br /> <br />
         
         <strong>You have likes {likesCount} times!</strong>
+        <p>{Massege}</p>
         </div>
 
         </main>)
